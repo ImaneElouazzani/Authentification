b0c3a28c486fcbbe7f2590ef5b751442c18da072
Imane Elouazzani <denjala.smiley@gmail.com>
2022-02-21 13:20:59 +0100

    creation home, navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css"
+import {Routes, Route} from "react-router-dom"
+import Home from "./Pages/Home";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="display-1">Bootstarp !</h1>
-    </div>
-  );
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    </>
+    );
 }
 
 export default App;
