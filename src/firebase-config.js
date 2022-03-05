c7810874f8ed430e92556ea20c5b99b861283b85
Imane Elouazzani <denjala.smiley@gmail.com>
2022-03-05 19:09:42 +0100

    données de la firebase sont changés

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: "G-VDDYMWS7X1"
+  measurementId: "G-JZYPTVTR3S"
 };
 
 // Initialize Firebase
